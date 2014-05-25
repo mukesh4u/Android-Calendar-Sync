321105d2da25243689e020793f135b12dbf2bf66
mkesh4u <mukesh421985@gmail.com>
2014-05-25 21:55:24 +0530

    Updated LICENSE of the code

--- CalendarView/src/com/examples/android/calendar/Utility.java
+++ CalendarView/src/com/examples/android/calendar/Utility.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2014 Mukesh Y authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.examples.android.calendar;
 
 import java.text.SimpleDateFormat;
@@ -8,6 +23,9 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 
+/**
+ * @author Mukesh Y
+ */
 public class Utility {
 	public static ArrayList<String> nameOfEvent = new ArrayList<String>();
 	public static ArrayList<String> startDates = new ArrayList<String>();
